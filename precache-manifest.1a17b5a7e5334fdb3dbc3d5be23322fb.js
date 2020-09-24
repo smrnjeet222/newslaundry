db8691b771d3f8b363255707da12624bdee555f5
Simranjeet Singh <smrnjeet222@gmail.com>
2020-09-24 18:07:25 +0530

    Updates

---
+++ precache-manifest.1a17b5a7e5334fdb3dbc3d5be23322fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f78c58acdc187eebd8d50e05f65f2c0",
+    "url": "/newslaundry/index.html"
+  },
+  {
+    "revision": "080bd00bb73a04790e39",
+    "url": "/newslaundry/static/css/main.42ef26a3.chunk.css"
+  },
+  {
+    "revision": "9efdc0f76078d6e7b79b",
+    "url": "/newslaundry/static/js/2.00ebf878.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newslaundry/static/js/2.00ebf878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080bd00bb73a04790e39",
+    "url": "/newslaundry/static/js/main.227ee0a2.chunk.js"
+  },
+  {
+    "revision": "fd667668337eb262a900",
+    "url": "/newslaundry/static/js/runtime-main.6e465499.js"
+  }
+]);
